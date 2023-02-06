e37f206d3294cb5235a42841dc44158a355518f7
dev-ximply <khairul@ximply.io>
2023-02-06 11:51:27 +0700

    budget

--- resources/views/budget.blade.php
+++ resources/views/budget.blade.php
@@ -230,7 +230,7 @@
 
                         $.ajaxSetup({
                             headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'
                             }
                         });
 
