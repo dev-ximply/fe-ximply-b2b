4d9eef440ee61d79d0c666e82891ddb512542ca7
dev-ximply <khairul@ximply.io>
2023-02-11 18:07:36 +0700

    approval

--- resources/views/old approval.blade.php
+++ resources/views/old approval.blade.php
@@ -100,7 +100,7 @@
                                                 <div class="ms-3 my-auto show-modal">
                                                     <div>
                                                         <span class="text-xs text-dark text-bold">
-                                                            {{ $expense_approval->sub_category_name }}
+                                                            {{ $expense_approval->category }}
                                                         </span>
                                                     </div>
                                                     <div>
@@ -117,7 +117,7 @@
                                             </div>
                                         </td>
                                         <td class="align-middle text-start text-xs text-capitalize text-dark">
-                                            {{ $expense_approval->category_name }}
+                                            {{ $expense_approval->category }}
                                         </td>
                                         <td class="align-middle text-start text-xs text-dark">
                                             {{ $expense_approval->purpose }}
@@ -146,7 +146,7 @@
                                             @if ($expense_approval->status == 'pending')
                                                 <div class="d-flex flex-row pt-3 d-flex justify-content-center">
                                                     <button
-                                                        onclick="getExpenseData('{{ $expense_approval->receipt_picture_directory }}', '{{ $expense_approval->additional_picture_directory }}', '{{ $expense_approval->receipt_date }}', '{{ $expense_approval->merchant }}', '{{ $expense_approval->total_amount }}', '{{ $expense_approval->location }}', '{{ $expense_approval->category_name }}', '{{ $expense_approval->sub_category_name }}', '{{ $expense_approval->client_name }}', '{{ $expense_approval->purpose }}', '{{ $expense_approval->expense_of }}', '{{ $expense_approval->status }}', '{{ $expense_approval->approval_id }}')"
+                                                        onclick="getExpenseData('{{ $expense_approval->receipt_picture_directory }}', '{{ $expense_approval->additional_picture_directory }}', '{{ $expense_approval->receipt_date }}', '{{ $expense_approval->merchant }}', '{{ $expense_approval->total_amount }}', '{{ $expense_approval->location }}', '{{ $expense_approval->category }}', '{{ $expense_approval->sub_category }}', '{{ $expense_approval->client_name }}', '{{ $expense_approval->purpose }}', '{{ $expense_approval->expense_of }}', '{{ $expense_approval->status }}', '{{ $expense_approval->approval_id }}')"
                                                         class="mx-1
                                                         btn text-white d-flex align-items-center d-flex
                                                         justify-content-center"
