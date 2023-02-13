9e0a87e5e5fc8f53021d88996a26fd94e544871d
dev-ximply <khairul@ximply.io>
2023-02-13 09:15:29 +0700

    update add member

--- resources/views/manage-teams/add-modal-user.blade.php
+++ resources/views/manage-teams/add-modal-user.blade.php
@@ -44,19 +44,23 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-6">
-                                <label class="form-label mt-2" style="color: black; font-weight:500">Group</label>
-                                <div class="">
-                                    <select class="form-control " name="department_id" id="department_id">
-                                        <option value="" selected>Select</option>
-                                        @foreach ($data['list_department'] as $item_departement)
-                                            <option value="{{ $item_departement->id }}">
-                                                {{ $item_departement->group_name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+
+                            @if (session()->get('is_superadmin') == true)
+                                <div class="col-6">
+                                    <label class="form-label mt-2" style="color: black; font-weight:500">Group</label>
+                                    <div class="">
+                                        <select class="form-control " name="department_id" id="department_id">
+                                            <option value="" selected>Select</option>
+                                            @foreach ($data['list_department'] as $item_departement)
+                                                <option value="{{ $item_departement->id }}">
+                                                    {{ $item_departement->group_name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
+
                             <div class="col-6">
                                 <label class="form-label mt-2" style="color: black; font-weight:500">Role</label>
                                 <div class="">
@@ -75,8 +79,15 @@
                                 <div class="mb-0">
                                     <button class="btn text-white" data-bs-dismiss="modal"
                                         style="background-color: #D42A34">Cancel</button>
-                                    <button type="sumbit" class="btn text-white" style="background-color: #62ca50"
-                                        onclick="sendInvitation('{{ Auth::user()['id'] }}', document.getElementById('email').value, document.getElementById('first_name').value, document.getElementById('last_name').value, document.getElementById('department_id').value, document.getElementById('role_id').value)">Submit</button>
+
+                                    @if (session()->get('is_superadmin') == true)
+                                        <button type="sumbit" class="btn text-white" style="background-color: #62ca50"
+                                            onclick="sendInvitation('{{ Auth::user()['id'] }}', document.getElementById('email').value, document.getElementById('first_name').value, document.getElementById('last_name').value, document.getElementById('department_id').value)">Submit</button>
+                                    @else
+                                        <button type="sumbit" class="btn text-white" style="background-color: #62ca50"
+                                            onclick="sendInvitation('{{ Auth::user()['id'] }}', document.getElementById('email').value, document.getElementById('first_name').value, document.getElementById('last_name').value)">Submit</button>
+                                    @endif
+
                                 </div>
                             </div>
                         </div>
@@ -92,7 +103,7 @@
 
 <script>
     //send invitation
-    function sendInvitation(user_id, email, first_name, last_name, group_id, role_id) {
+    function sendInvitation(user_id, email, first_name, last_name, role_id, group_id = null) {
         console.log(user_id);
         var tenant_code = TENANT_CODE;
         const swalWithBootstrapButtons = Swal.mixin({
@@ -122,11 +133,11 @@
                     formNewMember.append('email', email);
                     formNewMember.append('first_name', first_name);
                     formNewMember.append('last_name', last_name);
-                    if (group_id != "" || group_id != null) {
-                        formNewMember.append('group_id', group_id);
-                    }
                     if (role_id != "" || role_id != null) {
                         formNewMember.append('role_id', role_id);
+                    }
+                    if (group_id != "" || group_id != null) {
+                        formNewMember.append('group_id', group_id);
                     }
 
 
