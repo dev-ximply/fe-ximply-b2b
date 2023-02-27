d1702ffcdc49fc3734dafc83c42e6371c4d2231f
Khairul Kanters <khairulkanters@gmail.com>
2023-02-27 14:10:12 +0700

    bduget

--- resources/views/budget/edit-budget-admin.blade.php
+++ resources/views/budget/edit-budget-admin.blade.php
@@ -144,9 +144,9 @@
                             if ($("#main-loader")) {
                                 $("#main-loader").hide();
                             }
-                            // setTimeout(function() {
-                            //     location.reload();
-                            // }, 1000);
+                            setTimeout(function() {
+                                location.reload();
+                            }, 1000);
                         }
                     });
                 } else if (result.dismiss === Swal.DismissReason.cancel) {
